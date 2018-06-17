c0b94c19bf0fd9e7c48d62e41923458328416fc5
Anton <suddyadred@gmail.com>
2018-06-17 16:53:49 +0300

    Contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 jQuery(document).ready(function($) {
+    // the blocking of links default behavior
+    function prevent(){
+        $('.prevent, a[href="/broken"]').on('click', function(event){
+            event.preventDefault();
+        });
+    }
+    prevent();
+
 	// for smooth scroll
     smoothScroll.init({
         selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
@@ -6,4 +14,17 @@ jQuery(document).ready(function($) {
         easing: 'easeInQuad', // Easing pattern to use
         offset: 50 // Integer. How far to offset the scrolling anchor location in pixels
     });
+
+    //for input
+    if ($(".cwppartner-input-style").length) {
+        var input = $(".cwppartner-input-style");
+
+        input.focusout(function(){
+            if($(this).val() != ""){
+                $(this).addClass("has-content");
+            }else{
+                $(this).removeClass("has-content");
+            }
+        });
+    }
 });
